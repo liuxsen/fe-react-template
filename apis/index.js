ec9a1fce1bb2e7b01dec297a04272c3ea3ef12c4
刘建辉 <jianhui@yzyw-inc.com>
2019-12-07 00:29:00 +0800

    delete

--- apis/index.js
+++
@@ -1,1 +0,0 @@
-export { useIndexHttp } from './index/index';
--- redux/actions.js
+++
@@ -1,6 +0,0 @@
-import { Fetch_sku } from './actionTypes';
-
-export const FetchSku = skuList => ({
-  type: Fetch_sku,
-  payload: skuList
-});
